8f77505ba3908be6037eb310b2dd72b7c39a22b7
Mahdi Ruiz <RuizMahdi@gmx.com>
2023-11-21 13:30:19 -0700

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Search Web'
+copyright = '2023, Search Web'
+author = 'Search Web'
 
 release = '0.1'
 version = '0.1.0'
